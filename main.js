1369d9317c0972418f729cc32b9df2046c5742d9
guillaumepn <guillaumepn@free.fr>
2018-11-13 19:48:18 +0100

    get images from folder to JS + insert elements to DOM

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 import './styles/main.scss';
 
+let root = document.getElementById('root');
+
+let title = document.createElement('h1');
+title.innerText = 'Quick Kitchen';
+
+let timerImage = document.createElement('img');
+timerImage.src = 'images/timer.svg';
+
+root.append(title, timerImage);
